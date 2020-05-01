fe222c703d535e41a18c358f4f5f2b6afdc1cae2
azwyane <tunechibaral@gmail.com>
2020-05-01 16:24:57 +0545

    transient edit

--- pymux/Sensors.py
+++ pymux/Sensors.py
@@ -8,14 +8,50 @@ class sensor:
     def __init__(self):
         pass
 
-    def sensor(val):
-    if val=="all":
-        value=t.compute("termux-sensor -a")
-        return value["output"]
-   
-    elif val=="list":
-        value=t.compute("termux-sensor -l")
-        return value["output"]
+    def listSensor(self):
+        self.value=t.compute("termux-sensor -l")
+        return self.value["output"]
 
-       
-    #elif val=="sensor00"
+    def cleanup(self):
+        self.clean=t.compute("termux-sensor -c")
+        return self.clean["output"]
+
+    def delay(self,sensorname="",delayvalue=3000):
+        '''
+        Method to delay time in milliseconds
+        on receiving every new sensor update.
+        Arguments:
+        sensorname=""
+        delayvalue=3000(default)(delayed by 3 sec)
+        '''
+        self.sensorname=sensorname
+        self.delayvalue=delayvalue
+        self.delayv=t.compute(f"termux-sensor -s {self.sensorname} -d {self.delayvalue}")
+ 
+        return self.delayv["output"]
+
+    def specificSensors(self,sname=""):
+        '''
+        This is a method to print specific 
+        sensor(s) data.
+        Argument is either a single sensor
+        of list of sensors.
+        '''
+        
+        self.sname=sname
+        if self.sname=="":
+            self.display=t.compute(f"termux-sensor -s")
+            return self.display["output"]
+        else:
+            command="termux-sensor -s "+str([x for x in self.sname]).replace("[","").replace("]","").strip()
+            self.display=t.compute(command)
+            return self.display["output"]
+
+    
+
+    def allsensors(self):
+        '''
+        Method to print sensor data all at once.
+        WARNING: Can cause over load to the device.
+        '''
+        pass
